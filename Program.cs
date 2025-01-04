66e17aa96a5bb5af2c25c206b6a3775f2770e1ae
Dan Carroll <dancarroll@gmail.com>
2025-01-04 01:36:39 -0500

    Update to .NET Core, add option to ignore domains

--- Program.cs
+++ Program.cs
@@ -1,54 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Xml.Linq;
 
 namespace sitemap_diff
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            IEnumerable<string> originalUrls = PromptAndReadSitemap("Original: ");
-            IEnumerable<string> newUrls = PromptAndReadSitemap("New: ");
+      var ignoreDomain = Array.Exists(args, element => element == "--ignoreDomain");
 
-            IEnumerable<string> intersection = originalUrls.Intersect(newUrls);
+      IEnumerable<string> originalUrls = PromptAndReadSitemap("Original: ", ignoreDomain);
+      IEnumerable<string> newUrls = PromptAndReadSitemap("New: ", ignoreDomain);
 
-            IEnumerable<string> missingInNew = originalUrls.Where(s => !intersection.Contains(s));
-            IEnumerable<string> newInNew = newUrls.Where(s => !intersection.Contains(s));
+      IEnumerable<string> intersection = originalUrls.Intersect(newUrls);
 
-            Console.WriteLine("\nMissing in new sitemap");
-            Console.WriteLine("----------------------");
-            foreach (string s in missingInNew)
-            {
-                Console.WriteLine(s);
-            }
+      IEnumerable<string> missingInNew = originalUrls.Where(s => !intersection.Contains(s));
+      IEnumerable<string> newInNew = newUrls.Where(s => !intersection.Contains(s));
 
-            Console.WriteLine("\nNew in new sitemap");
-            Console.WriteLine("------------------");
-            foreach (string s in newInNew)
-            {
-                Console.WriteLine(s);
-            }
+      Console.WriteLine("\nMissing in new sitemap");
+      Console.WriteLine("----------------------");
+      foreach (string s in missingInNew)
+      {
+        Console.WriteLine(s);
+      }
 
-            Console.WriteLine();
-        }
+      Console.WriteLine("\nNew in new sitemap");
+      Console.WriteLine("------------------");
+      foreach (string s in newInNew)
+      {
+        Console.WriteLine(s);
+      }
 
-        static IEnumerable<string> PromptAndReadSitemap(string prompt)
-        {
-            Console.Write(prompt);
-            string url = Console.ReadLine();
-            
-            WebClient client = new WebClient();
-            string sitemapXml = client.DownloadString(url);
-
-            XNamespace ns = "http://www.sitemaps.org/schemas/sitemap/0.9";
-            XDocument xDoc = XDocument.Load(url);
-            var x = from el in xDoc.Element(ns + "urlset").Elements()
-                    select el.Element(ns + "loc").Value;
-
-            return x;
-        }
+      Console.WriteLine();
     }
+
+    static IEnumerable<string> PromptAndReadSitemap(string prompt, bool ignoreDomain)
+    {
+      Console.Write(prompt);
+      string url = Console.ReadLine();
+
+      WebClient client = new WebClient();
+      string sitemapXml = client.DownloadString(url);
+
+      XNamespace ns = "http://www.sitemaps.org/schemas/sitemap/0.9";
+      XDocument xDoc = XDocument.Load(url);
+      var x = from el in xDoc.Element(ns + "urlset").Elements()
+              select (ignoreDomain ?
+                new Uri(el.Element(ns + "loc").Value).PathAndQuery
+                : el.Element(ns + "loc").Value);
+
+      return x;
+    }
+  }
 }
